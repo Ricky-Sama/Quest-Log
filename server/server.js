439949e7b5309110aae01d034e4db3f86b85e5ad
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-11-30 19:17:52 -0500

    moved files, added folders and server.js/connection.js files

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const router = require("./routes")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const db = require('./config/connection')
+
+app.use("/", router)
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Server is listening port ${PORT}`))
+})
