737d94645ab935298ee8bc36f9c35bf713852727
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-12-04 20:41:51 -0500

    added semicolons to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 import { Route, Switch } from "react-router-dom";
-import Login from './pages/login_page'
-import Register from './pages/register_page'
+import Login from './pages/login_page';
+import Register from './pages/register_page';
 
 function App() {
   return (
