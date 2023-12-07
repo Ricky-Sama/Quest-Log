e114bd05560659ba26af5f56c95fb294b8d6ef47
Kevin Rice <reatriniti@gmail.com>
2023-12-07 07:06:31 -0500

    npm tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: (_) => ({
+        'custom-background': "url('/src/assets/Images/Quest-1.jpg')"
+      }),
+    },
+  },
+  plugins: [],
+}
