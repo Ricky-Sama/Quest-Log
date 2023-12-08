924d6c71a004504a018c3bf21a32d68051c92da7
Devop-A <hunter.austin407@gmail.com>
2023-12-07 23:39:29 -0500

    adding variables for calendarwrapper

--- client/src/components/calendarwrapper/calendarwrapper.jsx
+++ client/src/components/calendarwrapper/calendarwrapper.jsx
@@ -2,3 +2,6 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './CalendarWrapper.css';
+
+const CalendarWrapper = () => {
+    const [value, onChange] = useState(new Date());
