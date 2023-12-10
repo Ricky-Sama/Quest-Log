e39e36269b794b6de59da9f0004a8d2e8bf82769
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-12-10 17:28:44 -0500

    revised calendarwrapper.css to display lowercase letters in calendarwrapper.jsx

--- client/src/components/calendarwrapper/calendarwrapper.jsx
+++ client/src/components/calendarwrapper/calendarwrapper.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './CalendarWrapper.css';
+import './calendarwrapper.css';
 
 const CalendarWrapper = () => {
     const [value, onChange] = useState(new Date());
