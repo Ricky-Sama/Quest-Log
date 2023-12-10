c0aeb2436d85787008181ee79c2c1b5de5a30e00
Kevin Rice <reatriniti@gmail.com>
2023-12-10 09:51:05 -0500

    updated Note

--- client/src/pages/Note.jsx
+++ client/src/pages/Note.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react'; //
+import { FaRegTrashCan } from "react-icons/fa6";
+import { MdEditNote } from "react-icons/md";
 
 function Note() {
   const [notes, setNotes] = useState([
@@ -7,7 +9,7 @@ function Note() {
     // Add more initial quests if needed
   ]);
 
-  const [journalEntries, setJournalEntries] = useState([
+  const [journalEntries, setJournalEntries] = useState([ ////////////// creates the initial journal entries ////////////////////////////////////////////////////////////////////////////////////////////
     { id: 1, type: 'journal', title: 'Sample Journal Entry 1', content: 'Lorem ipsum dolor sit amet.' },
     { id: 2, type: 'journal', title: 'Sample Journal Entry 2', content: 'Consectetur adipiscing elit.' },
     // Add more initial journal entries if needed
@@ -34,12 +36,21 @@ function Note() {
       setNewJournalContent('');
     }
   };
-
+////////////////// editNote function ////////////////////////////////////////////////////////////////////////////////////////////
   const editNote = (id, type) => {
-    // Implement edit functionality here
-    // You may open a modal or update the state to allow editing
+    if (type === 'quest') {
+      const noteToEdit = notes.find((note) => note.id === id);
+      setNewNote(noteToEdit.text);
+      setNotes(notes.filter((note) => note.id !== id));
+    } else if (type === 'journal') {
+      const entryToEdit = journalEntries.find((entry) => entry.id === id);
+      setNewJournalTitle(entryToEdit.title);
+      setNewJournalContent(entryToEdit.content);
+      setJournalEntries(journalEntries.filter((entry) => entry.id !== id));
+    }
   };
 
+////////////////// deleteNote function ////////////////////////////////////////////////////////////////////////////////////////////  
   const deleteNote = (id, type) => {
     if (type === 'quest') {
       setNotes(notes.filter((note) => note.id !== id));
@@ -47,30 +58,30 @@ function Note() {
       setJournalEntries(journalEntries.filter((entry) => entry.id !== id));
     }
   };
-
+////////////////// tailwindcss styling ////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className='min-h-screen flex flex-col items-center justify-center gap-4 p-4 bg-custom-background bg-center bg-cover'>
-      <div className='bg-gray-100 bg-opacity-80 p-6 rounded shadow-lg w-full max-w-lg lg:w-1/4'>
+      <div className='bg-gray-100 bg-opacity-70 p-6 rounded-lg shadow-black-300 w-full max-w-lg lg:w-1/4'>
         <h1 className='text-3xl font-bold text-center mb-4'>Quest Log</h1>
         <div className='flex flex-col mb-4'>
           <input
             type="text"
             placeholder='Journal title'
-            className='py-2 px-4 border rounded mb-2 focus:outline-none'
+            className='py-2 px-4 border rounded mb-2 focus:outline-none shadow-lg'
             value={newJournalTitle}
             onChange={(e) => setNewJournalTitle(e.target.value)}
           />
           <textarea
             placeholder='Journal content'
-            className='py-2 px-4 border rounded h-20 focus:outline-none'
+            className='py-2 px-4 border rounded h-20 mb-2 focus:outline-none shadow-lg'
             value={newJournalContent}
             onChange={(e) => setNewJournalContent(e.target.value)}
           />
           <button
-            className='bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded'
+            className='bg-gradient-to-r from-blue-400 to-blue-600 text-white py-2 px-4 rounded shadow-lg hover:from-blue-600 hover:to-blue-800'
             onClick={addJournalEntry}
           >
-            Add Journal Entry
+            Add Quest Entry
           </button>
         </div>
         <ul>
@@ -82,14 +93,16 @@ function Note() {
                 <button
                   className='mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounded hover:to-gray-700'
                   onClick={() => editNote(entry.id, entry.type)}
+                  title='Edit'
                 >
-                  Edit
+                <MdEditNote />
                 </button>
                 <button
                   className='mr-2 p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounded hover:to-gray-700'
                   onClick={() => deleteNote(entry.id, entry.type)}
+                  title='Delete'
                 >
-                  Remove
+                <FaRegTrashCan /> 
                 </button>
               </div>
             </li>
