4a56b18326d5b0fd9ae521352b03298601670e29
Jesse <jesse.lare@gmail.com>
2023-12-10 18:03:54 -0500

    adding in bcrypt, completing 'create new user with hashed password

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
-
 const { User } = require('../../models');
 
 // Get all users
@@ -14,14 +14,21 @@ router.get('/', async (req, res) => {
 })
 
 
-// Create a new user
+// Create a new user (with hashed password )
 router.post('/', async (req, res) => {
     try {
-        const newUser = await User.create(req.body)
-        res.json(newUser)
+        const { password, ...otherDetails } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt
+        const newUser = await User.create({ password: hashedPassword, ...otherDetails });
+        res.json(newUser);
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        res.status(500).json({ error: error.message });
     }
-})
+});
+
+
+    }
+
+
 
 module.exports = router;
