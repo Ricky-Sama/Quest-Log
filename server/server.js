4ddb09e0a3f922bb0e86cc8dce9ce83996feb0dc
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-12-12 21:27:59 -0500

    updated destination in middleware for server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", router)
 
 // Serve static assets in production (We can configure this when we have a build folder)
-app.use(express.static("client/build"));
+// app.use(express.static("..client/build"));
+app.use(express.static("..client/dist"));
 
 // Connect to the database and server
 db.once('open', () => {
