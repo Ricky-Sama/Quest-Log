4ed58c147ad775c987129fc7cd69cb7e9e1f7574
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-12-12 19:34:15 -0500

    updated destination for index.js in routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,12 +7,12 @@ const path = require("path")
 router.use('/api', apiRouter)
 
 //Static client views
-router.use(express.static(path.join(__dirname, '../../client')));
+router.use(express.static(path.join(__dirname, '../../client/dist')));
 
 
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 module.exports = router;
