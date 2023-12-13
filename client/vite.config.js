c84ca885051728f504d59851a4707e917ec0cbcb
Devop-A <hunter.austin407@gmail.com>
2023-12-13 13:58:20 -0500

    denug

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-          external: ['react-icons/bs', 'react-icons/fa', 'react-icons/gi','react-icons/md','react-calendar','react-calendar/dist/Calendar.css','react-icons/tb','react-icons/ri','react-icons/gi'],
-        },
-    },
+    
 });
