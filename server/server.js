0ef98c9a76e073e4de7dc179a08dd14ea05273e2
Devop-A <hunter.austin407@gmail.com>
2023-12-13 22:39:09 -0500

    debug

--- server/server.js
+++ server/server.js
@@ -6,29 +6,24 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3000;
 const router = require("./routes");
 const session = require('express-session');
-const path = require('path'); // Added to handle paths
 
 // Middleware to parse JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: process.env.SESSION_SECRET, 
+    secret: process.env.SESSION_SECRET, // Ensure this environment variable is set in Heroku
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: process.env.NODE_ENV === 'production' } // Simplified the secure flag
+    cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
 
-// Use the main router
+// Use routes
 app.use("/", router);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build'))); // Corrected the path
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html')); // Serve index.html for all unknown routes
-    });
+    app.use(express.static("../client/build"));
 }
 
 // Connect to the database and server
